34e457c082145ad63e5f1f45020d8541dd64552b
Copperage <ethanhud6@gmail.com>
2023-12-21 15:03:02 +0000

    update

---
+++ README.md
@@ -0,0 +1,3 @@
+# TOP-to-do-list
+
+Figma: ![image](https://github.com/Copperage/TOP-to-do-list/assets/72754487/bc22a62a-06c6-4219-a250-d382654ee85b)
---
+++ webpack.config.js
@@ -0,0 +1,27 @@
+const path = require('path');
+
+module.exports = {
+	mode: 'development',
+	entry: path.resolve(__dirname, 'src/index.js'),
+	output: {
+		path: path.resolve(__dirname, 'dist'),
+	},
+	devServer: {
+		static: {
+			directory: path.resolve(__dirname, 'dist'),
+		},
+		port: 3000,
+		open: true,
+		hot: true,
+		compress: true,
+		historyApiFallback: true,
+	},
+	module: {
+		rules: [
+			{
+				test: /\.scss$/,
+				use: ['style-loader', 'css-loader', 'sass-loader'],
+			},
+		],
+	},
+};
