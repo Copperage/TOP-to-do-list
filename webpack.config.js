2cb6cf305554741b6d264d00e106dbc11923043e
Ethan <ethanhud6@gmail.com>
2024-01-05 16:32:57 +0000

    svgs spent way too long messing about just to use inline anyway

--- webpack.config.js
+++ webpack.config.js
@@ -22,6 +22,14 @@ module.exports = {
 				test: /\.scss$/,
 				use: ['style-loader', 'css-loader', 'sass-loader'],
 			},
+			{
+				test: /\.svg$/,
+				use: [
+					{
+						loader: 'svg-inline-loader',
+					},
+				],
+			},
 		],
 	},
 };
