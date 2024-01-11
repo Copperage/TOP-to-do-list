21492915a441852c8351fa41fcf27972a786eb6f
Ethan <ethanhud6@gmail.com>
2024-01-11 16:41:41 +0000

    converted html to javascript only layout so far

--- src/index.js
+++ src/index.js
@@ -1,1 +1,8 @@
 import './styles/main.scss';
+import initWebsite from './modules/page-init';
+// import starSvg from './modules/svgs/star-f.svg';
+// import plusSvg from './modules/svgs/plus.svg';
+// import tickboxSvg from './modules/svgs/Tickbox.svg';
+// import crossboxSvg from './modules/svgs/Crossbox.svg';
+
+initWebsite();
