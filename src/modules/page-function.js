76e8b1220f05108ca65aeaeab2cfae41c55405a8
Ethan <ethanhud6@gmail.com>
2024-01-25 18:55:53 +0000

    added add button

---
+++ src/modules/page-function.js
@@ -0,0 +1,9 @@
+import initWebsite from './page-content';
+import { createCards } from './page-content';
+
+function initFunctionality() {}
+
+initFunctionality();
+initWebsite();
+
+export default initFunctionality;
